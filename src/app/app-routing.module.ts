fa95472d6e4dd24d9b09d24b4b5a8bb110b166e7
lashagelashvili <las.gelashvili@gmail.com>
2021-12-15 02:11:52 +0400

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './components/cards/cards.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CardsComponent },
+  { path: 'user', component: UserDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
