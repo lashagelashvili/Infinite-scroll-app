d89ee7da8cdebe0e54f2063df7a485c2b1be46a2
lashagelashvili <las.gelashvili@gmail.com>
2021-12-16 21:47:10 +0400

    user details breadcrumbs and spinner

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,47 +1,70 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { InfiniteScrollDirective } from 'src/app/directives/infinite-scroll.directive';
 import { ApiService } from 'src/app/services/api.service';
 import { User, UserDetails, UserFriends } from 'src/app/services/model';
 
+interface BreadCrumb {
+  fullname: string,
+  id: number;
+}
+
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent implements OnInit, AfterViewInit {
+export class UserDetailsComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChildren('item', { read: ElementRef })
   items: QueryList<ElementRef>;
   @ViewChild(InfiniteScrollDirective) directive: InfiniteScrollDirective;
 
   routeSub: Subscription;
-
   userId: number;
   userDetails: UserDetails;
   userFriends: User[] = [];
   friendsPage = 1;
+  breadCrumbs: BreadCrumb[] = [];
+  loadingUser: boolean = false;
+  loadingFriends: boolean = false;
 
   constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.routeSub = this.route.params.subscribe(params => {
+      this.friendsPage = 1;
+      this.userFriends = [];
       this.userId = params['id'];
+      this.getUserDetails();
+      this.getFriends();
     })
-    this.getUserDetails();
-    // console.log(this.userDetails)
-    this.getFriends();
+  }
+
+  ngAfterViewInit(): void {
+    this.directive.updateItems(this.items);
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
   }
 
   getUserDetails() {
+    this.loadingUser = true;
     this.apiService.getUserDetails(this.userId).subscribe((data: UserDetails) => {
+      this.loadingUser = false;
       this.userDetails = data;
       this.userDetails.imageUrl = this.userDetails.imageUrl + '?v=' + this.userId;
+      if (!this.breadCrumbs.some(breadcrumb => breadcrumb.id === data.id)) {
+        this.breadCrumbs.push({ fullname: `${data.prefix} ${data.name} ${data.lastName} `, id: data.id });
+      }
     })
   }
 
   getFriends() {
+    this.loadingFriends = true;
     this.apiService.getFriends(this.userId, this.friendsPage).subscribe((data: UserFriends) => {
+      this.loadingFriends = false;
       data.list.forEach((friend: User) => {
         this.userFriends.push(friend);
         friend.imageUrl = friend.imageUrl + '?v=' + friend.id
@@ -50,24 +73,14 @@ export class UserDetailsComponent implements OnInit, AfterViewInit {
     })
   }
 
-  ngAfterViewInit(): void {
-    this.directive.updateItems(this.items);
-    // console.log(this.userFriends)
+  onClick(id: number) {
+    this.router.navigateByUrl(`/user/${id}`);
   }
 
-  onFriendClick(id: number) {
-    console.log(id)
-    this.router.navigateByUrl(`user/${id}`);
-    this.userId = id;
-    this.friendsPage = 1;
-    this.userFriends = [];
-    this.getUserDetails();
-    this.getFriends();
-    // this.directive.updateItems(this.items);
-    // this.route.params.subscribe(params => {
-    //   id = params['id'];
-    // })
+  onBreadCrumbClick(id: number) {
+    const breadCrumbIndex = this.breadCrumbs.findIndex(breadcrumb => breadcrumb.id === id);
+    this.breadCrumbs = this.breadCrumbs.filter((breadCrumb, index) => index <= breadCrumbIndex);
+    this.router.navigateByUrl(`/user/${id}`);
   }
-
 
 }
