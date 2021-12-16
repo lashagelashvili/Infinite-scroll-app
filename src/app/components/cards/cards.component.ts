bb88f4dc8e6aa5e8594544baf880c7bf6e7b9b1c
lashagelashvili <las.gelashvili@gmail.com>
2021-12-16 15:38:06 +0400

    infinite scroll with directive

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,6 +1,8 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
+import { InfiniteScrollDirective } from 'src/app/directives/infinite-scroll.directive';
 import { ApiService } from 'src/app/services/api.service';
+import { User, UserFriends } from 'src/app/services/model';
 
 @Component({
   selector: 'app-cards',
@@ -10,62 +12,41 @@ import { ApiService } from 'src/app/services/api.service';
 export class CardsComponent implements OnInit, AfterViewInit {
   @ViewChildren('item', { read: ElementRef })
   items: QueryList<ElementRef>;
+  @ViewChild(InfiniteScrollDirective) directive: InfiniteScrollDirective;
 
   loading: boolean = false;
   currentPage: number = 1;
-  users: any = [];
+  userId: number = 1;
+  users: User[] = [];
   totalPages: number;
   obsever: IntersectionObserver;
-  private lastElement: ElementRef | undefined;
 
-  constructor(private stateService: ApiService, private router: Router) { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit(): void {
     this.getUsers();
-    this.intersectionObserver();
   }
 
   ngAfterViewInit() {
-    this.items.changes.subscribe((data) => {
-      if (this.lastElement !== undefined) {
-        this.obsever.unobserve(this.lastElement.nativeElement)
-      }
-      if (data.last) {
-        this.lastElement = data.last;
-        this.obsever.observe(data.last.nativeElement)
-      }
-    })
+    this.directive.updateItems(this.items);
   }
 
   getUsers() {
     this.loading = true;
-    this.stateService.getData(this.currentPage).subscribe((data) => {
+    this.apiService.getData(this.currentPage).subscribe((data: UserFriends) => {
       this.loading = false;
-      data.list.forEach((item: any) => {
-        this.users.push(item)
+      console.log(data)
+      data.list.forEach((item: User) => {
+        item.imageUrl = item.imageUrl + '?v=' + this.userId;
+        this.userId++;
+        this.users.push(item);
       })
-
       this.totalPages = data.pagination.total;
       this.currentPage++;
     })
   }
 
-
-  intersectionObserver() {
-    let options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.25,
-    }
-
-    this.obsever = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        this.getUsers();
-      }
-    }, options);
-  }
-
-  onUserClick() {
-    this.router.navigateByUrl('user');
+  onUserClick(id: number) {
+    this.router.navigateByUrl(`user/${id}`);
   }
 }
--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { InfiniteScrollDirective } from 'src/app/directives/infinite-scroll.directive';
+import { ApiService } from 'src/app/services/api.service';
+import { User, UserDetails, UserFriends } from 'src/app/services/model';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent implements OnInit, AfterViewInit {
+  @ViewChildren('item', { read: ElementRef })
+  items: QueryList<ElementRef>;
+  @ViewChild(InfiniteScrollDirective) directive: InfiniteScrollDirective;
 
-  constructor() { }
+  routeSub: Subscription;
+
+  userId: number;
+
+  userDetails: UserDetails;
+  userFriends: User[] = [];
+  friendsPage = 1;
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.userId = params['id'];
+    })
+    this.getUserDetails();
+    // console.log(this.userDetails)
+    this.getFriends();
   }
 
+  getUserDetails() {
+    this.apiService.getUserDetails(this.userId).subscribe((data) => {
+      this.userDetails = data;
+      this.userDetails.imageUrl = this.userDetails.imageUrl + '?v=' + this.userId;
+    })
+  }
+
+  getFriends() {
+    this.apiService.getFriends(this.userId, this.friendsPage).subscribe((data: UserFriends) => {
+      data.list.forEach((friend: User) => {
+        this.userFriends.push(friend);
+        friend.imageUrl = friend.imageUrl + '?v=' + friend.id
+      })
+      this.friendsPage++;
+    })
+  }
+
+  ngAfterViewInit(): void {
+    this.directive.updateItems(this.items);
+    // console.log(this.userFriends)
+  }
 }
